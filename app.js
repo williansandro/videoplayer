63d17a4a9c453c05183e6286d7d53981f648d9b7
willian sandro <31625826+williansandro@users.noreply.github.com>
2019-12-17 15:15:51 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,61 @@
 const song = document.querySelector(".song");
-const play = document.querySelector(".play");
+const play1 = document.querySelector(".play1");
+const play2 = document.querySelector(".play2");
+const play3 = document.querySelector(".play3");
 const replay = document.querySelector(".replay");
 const video = document.querySelector(".vid-container video");
+const mostrar = document.querySelector(".vid-container");
+
 
 const sounds = document.querySelectorAll(".sound-picker button");
 
 sounds.forEach(sound => {
-    sound.addEventListener("click", function() {
-      song.src = this.getAttribute("data-sound");
-      video.src = this.getAttribute("data-video");
-      
+    sound.addEventListener("click", function () {
+        song.src = this.getAttribute("data-sound");
+        video.src = this.getAttribute("data-video");
+
     });
-  });
-  
-  play.addEventListener("click", () => {
-    song.play();
-  });
+});
 
-  replay.addEventListener("click", function() {
-    restartSong(song);
-    
-  });
+play1.addEventListener("click", () => {
+    if (video.paused) {
+        video.play();
+
+    } else {
+        video.pause();
+
+    }
+});
+
+play2.addEventListener("click", () => {
+    video.play();
+});
+
+play3.addEventListener("click", () => {
+    video.play();
+});
+
+replay.addEventListener("click", () => {
+    restartSong(video);
+
+});
 
 
-  const restartSong = song =>{
+
+const restartSong = song => {
     let currentTime = song.currentTime;
     song.currentTime = 0;
     console.log("ciao")
 
 }
 
-const checkPlaying = song => {
-    if (song.paused) {
-      song.play();
-      video.play();
-      play.src = "./svg/pause.svg"
-    } else {
-      song.pause();
-      video.pause();
-      
-    }
-  };
+function visivel() {
+    mostrar.style.display = "block";
+
+}
+
+
+
+
+
+
